9b72ee2f5480421b06ad60e360acb270666e88fb
Raphaël Talbot <2035556@cegepba.qc.ca>
2022-02-04 10:27:37 -0500

    a

--- frontend/src/pages/menu.js
+++ frontend/src/pages/menu.js
@@ -1,13 +1,28 @@
 import react from "react";
+import './menu.css';
 
 const Menu = () =>{
     return(
         <div className="menu">
-            <div className="left">
-                <p>a</p>
+            <div className="left part">
+                <div className="title">
+                    <p>Main dish</p>
+                </div>
+                <ul>
+                    <li>Poulet fri</li>
+                </ul>
             </div>
             <div className="right">
-                <p>b</p>
+            <div className="top">
+                <div className="title">
+                    <p>Appetizer</p>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="title">
+                    <p>Dessert</p>
+                </div>
+            </div>
             </div>
         </div>
     )

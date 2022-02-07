c1e4ccf5affbe94bccf0ace52fa193c34ac0dd8d
raixonomas <83031889+raixonomas@users.noreply.github.com>
2022-02-07 09:42:10 -0500

    t

--- frontend/src/pages/reservation.js
+++ frontend/src/pages/reservation.js
@@ -9,6 +9,7 @@ class EmailForm extends React.Component {
             noTelephone : "",
             email : "",
             message : "",
+            personne : null,
             date : null,
             hour : null
         }
@@ -20,12 +21,18 @@ class EmailForm extends React.Component {
       }
 
     /*handleChange(e) {
+        const value = e.value
+        
         this.setState({
-            {e.id}
+            e.id : value 
         })
-
+        console.log()
     }*/
     
+    sendEmail() {
+
+    }
+
     render() {
         return(
             <div className="emailForm">
@@ -33,19 +40,19 @@ class EmailForm extends React.Component {
                     <div className="inputPI">
                         <label>
                             Nom complet:
-                            <input type={"text"} id="fullname" name="fullname" onChange={(e) => this.handleChange(e)}></input>
+                            <input type={"text"} id="fullname" name="fullname" onChange={(e) => this.setState({ fullname : e.target.value})}></input>
                         </label>
                     </div>
                     <div className="inputPI">
                         <label>
                             Numéro de téléphone :
-                            <input type={"tel"} id="telephone" name="telephone" onChange={(e) => this.handleChange(e)}></input>
+                            <input type={"tel"} id="noTelephone" name="telephone" onChange={(e) => this.setState({ noTelephone : e.target.value})}></input>
                         </label>
                     </div>
                     <div className="inputPI">
                         <label>
                             Adresse courriel :
-                            <input type={"email"} id="email" name="email" onChange={(e) => this.handleChange(e)}></input>
+                            <input type={"email"} id="email" name="email" onChange={(e) => this.setState({ email : e.target.value})}></input>
                         </label>
                     </div>
                 </div>
@@ -53,21 +60,27 @@ class EmailForm extends React.Component {
                     <div className="message">
                     <label>
                         Message:
-                        <input type={"text"} id="message" name="message" onChange={(e) => this.handleChange(e)}></input>
+                        <input type={"text"} id="message" name="message" onChange={(e) => this.setState({ message : e.target.value})}></input>
+                    </label>
+                    </div>
+                    <div className="personne">
+                    <label>
+                        Nombre de personne:
+                        <input type={"number"} id="personne" name="personne" onChange={(e) => this.setState({ personne : e.target.value})}></input>
                     </label>
                     </div>
                     <div className="date-hour">
                     <label>
-                        Date:
-                        <input type={"date"} id="date" name="date" onChange={(e) => this.handleChange(e)}></input>
+                        Date:                     
+                        <input type={"date"} id="date" name="date" onChange={(e) => this.setState({ date : e.target.value})}></input>
                     </label>
                     <label>
                         Heure:
-                        <input type={"time"} id="hour" name="hour" onChange={(e) => this.handleChange(e)}></input>
+                        <input type={"time"} id="hour" name="hour" onChange={(e) => this.setState({ hour : e.target.value})}></input>
                     </label>
                     </div>
                     <div className="button">
-                        <input type={"submit"} id="sumbit" name="submit" value={"Envoyer la réservation"}></input>
+                        <input type={"submit"} id="sumbit" name="submit" value={"Envoyer la réservation"} onClick={() => this.sendEmail()}></input>
                     </div>
                 </div>   
             </div>
